16df7a91082cd206fecd7db0df7f20a45ec30e74
Eduardo López <loama18@gmail.com>
2023-10-06 18:13:01 -0600

    fix

--- manifest/create-tar.sh
+++ manifest/create-tar.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# Command 1
-rsync -av --progress ../ ./tmp/temp_directory/ --exclude node_modules/ --exclude app/.nuxt/ --exclude app/.node_modules/
+# Define the root directory
+ROOT_DIR="$(dirname $(dirname $(realpath $0)))"
 
-# Command 2
-tar -czvf ./manifest/heroku.tgz -C /tmp temp_directory/
+# Temporary directory for rsync
+TMP_DIR="$ROOT_DIR/tmp/temp_directory"
 
-# Command 3
-rm -rf ./tmp/temp_directory/
+# Ensure the temporary directory exists
+mkdir -p "$TMP_DIR"
+
+# Command 1: rsync to copy excluding the specified folders
+rsync -av --progress "$ROOT_DIR/" "$TMP_DIR/" --exclude .git/ --exclude node_modules/ --exclude app/.nuxt/ --exclude app/.node_modules/
+
+# Command 2: Create the tarball
+tar -czvf "$ROOT_DIR/manifest/heroku.tgz" -C "$TMP_DIR" .
+
+# Command 3: Cleanup
+rm -rf "$TMP_DIR"
