cf7d357a133fa98654e18ab9614242d57a452167
Eduardo López <loama18@gmail.com>
2023-10-07 00:16:51 -0600

    update deps

--- manifest/firebase.js
+++ manifest/firebase.js
@@ -1,8 +1,8 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./manifest-hq-firebase-adminsdk-5um2c-5e3221afd0.json');
+const admin = require('firebase-admin')
+const serviceAccount = require('./manifest-hq-firebase-adminsdk-5um2c-5e3221afd0.json')
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+  credential: admin.credential.cert(serviceAccount)
+})
 
-module.exports = admin;
+module.exports = admin
--- manifest/index.js
+++ manifest/index.js
@@ -1,22 +1,22 @@
-const fs = require('fs');
-const admin = require('./firebase.js');
+const fs = require('fs')
+const admin = require('./firebase.js')
 
-const firestore = admin.firestore();
+const firestore = admin.firestore()
 
 // Query the Firestore collection
-const filesRef = firestore.collection('files');
-const filesQuery = filesRef.where('project', '!=', 'CA');
+const filesRef = firestore.collection('files')
+const filesQuery = filesRef.where('project', '!=', 'CA')
 
 filesQuery.onSnapshot((querySnapshot) => {
   querySnapshot.forEach((doc) => {
-    const data = doc.data();
-    console.log(`Document data: ${JSON.stringify(data)}`);
+    const data = doc.data()
+    console.log(`Document data: ${JSON.stringify(data)}`)
 
-    const filePath = data.path;
-    fs.writeFileSync('app/' + filePath, `${data.value}`);
-  });
+    const filePath = data.path
+    fs.writeFileSync('app/' + filePath, `${data.value}`)
+  })
 }, (error) => {
-  console.error(`Error fetching documents: ${error}`);
-});
+  console.error(`Error fetching documents: ${error}`)
+})
 
 console.log('started')
