7edafec894d57eb073511ad9e96d51f889847654
Eduardo López <loama18@gmail.com>
2023-10-07 16:56:48 -0600

    use env variable

--- app/components/Example.vue
+++ app/components/Example.vue
@@ -1,1 +1,11 @@
-<template>example</template>
+<template>
+  <div>
+    {{ GITHUB_BRANCH }}
+  </div>
+</template>
+
+<script setup>
+const config = useRuntimeConfig()
+
+const GITHUB_BRANCH = config.NUXT_PUBLIC_GITHUB_BRANCH
+</script>
--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      GITHUB_REPO: process.env.NUXT_PUBLIC_GITHUB_REPO,
+      GITHUB_BRANCH: process.env.NUXT_PUBLIC_GITHUB_BRANCH,
+      PROJECT_ID: process.env.NUXT_PUBLIC_PROJECT_ID
+    }
+  },
   ssr: false
 })
