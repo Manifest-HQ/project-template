099b157056310a106e88a415330693b3fa24073e
Eduardo López <loama18@gmail.com>
2024-03-08 14:01:02 +0100

    update

--- manifest/README.md
+++ manifest/README.md
@@ -4,6 +4,3 @@ This repo contains a NodeJS project which in turn, contains a nuxt project used 
 
 It is supposed to be hosted on heroku and run the nuxt app in dev mode.
 Connected to firestore it would update files of the nuxt app and reload things.
-
-## Update build
-Run from terminal `./manifest/create-tar.sh`
---
+++ manifest/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://dxusbfemztqipqvlneat.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4dXNiZmVtenRxaXBxdmxuZWF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU4NjUyMDgsImV4cCI6MjAxMTQ0MTIwOH0.xlpgbon7iolkJa6YB-E1hXHQFyDdN4FDVZBg6To0sos' // process.env.SUPABASE_KEY
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
