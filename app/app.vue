d914e08cf8db1aee47bf8ae7d0f3a607354b19e4
Eduardo López <loama18@gmail.com>
2024-03-13 20:10:04 +0100

    a

--- app/app.vue
+++ app/app.vue
@@ -12,7 +12,24 @@ import { CapacitorUpdater } from '@capgo/capacitor-updater'
 onMounted(() => {
   // TODO use this only on ios and android, not web
   CapacitorUpdater.notifyAppReady()
+  initIframeMessaging()
 })
+
+function initIframeMessaging() {
+  document.addEventListener('click', function(event) {
+    // Optional: Check if the clicked element is the one you're interested in
+    const tagID = event.target.getAttribute('tag-id');
+    if (!tagID) return
+    // Send a message to the parent window with the clicked element's ID
+    window.parent.postMessage({
+      type: 'elementClicked',
+      tagID: tagID,
+      className: event.target.className,
+      tagName: event.target.tagName,
+      innerText: event.target.innerText
+    }, '*')
+  })
+}
 </script>
 
 <style lang="sass">
