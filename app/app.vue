cdd333117066acf19c0eab9c71a02cae4913f0c0
Eduardo López <loama18@gmail.com>
2024-04-01 22:02:58 +0200

    send message on mounted

--- app/app.vue
+++ app/app.vue
@@ -23,29 +23,35 @@ onMounted(() => {
   // TODO use this only on ios and android, not web
   CapacitorUpdater.notifyAppReady()
   initIframeMessaging()
+  sendMessageHTMLBody()
 })
 
 function initIframeMessaging() {
   document.addEventListener('click', function(event) {
-    console.log('click', event)
-    // Optional: Check if the clicked element is the one you're interested in
-    const tagID = event.target.getAttribute('tag-id')
-    if (!tagID) return
-    // Send a message to the parent window with the clicked element's ID
-    window.parent.postMessage({
-      type: 'elementClicked',
-      tagID: tagID,
-      className: event.target.className,
-      tagName: event.target.tagName,
-      innerText: event.target.innerText
-    }, '*')
-
-    const htmlBody = document.body
-    window.parent.postMessage({
-      type: 'htmlBody',
-      htmlBody: htmlBody.innerHTML
-    }, '*')
+    sendMessageElementClicked(event)
+    sendMessageHTMLBody()
   })
+}
+
+function sendMessageElementClicked (event) {
+  const tagID = event.target.getAttribute('tag-id')
+  if (!tagID) return
+  // Send a message to the parent window with the clicked element's ID
+  window.parent.postMessage({
+    type: 'elementClicked',
+    tagID: tagID,
+    className: event.target.className,
+    tagName: event.target.tagName,
+    innerText: event.target.innerText
+  }, '*')
+}
+
+function sendMessageHTMLBody() {
+  const htmlBody = document.body
+  window.parent.postMessage({
+    type: 'htmlBody',
+    htmlBody: htmlBody.innerHTML
+  }, '*')
 }
 </script>
 
