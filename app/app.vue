2f5f71bd07e44b3099051dfff4a1870e864f6ea9
Eduardo López <loama18@gmail.com>
2024-04-02 17:42:45 +0200

    update

--- app/app.vue
+++ app/app.vue
@@ -1,26 +1,5 @@
 <template>
   <div class="h-screen" tag-id="tag-4F57HrTt">
-    <div class="pt-safe bg-blue-400 p-2" tag-id="tag-LwLddRu5">
-      <div tag-id="tag-CHYIGDnb">level 1</div>
-      <div tag-id="tag-H1GF8SUG">
-        <span tag-id="tag-hvfmKtUz">level 2</span>
-        <div tag-id="tag-rzBmtjqq">level 3</div>
-        <img src="https://via.placeholder.com/150" tag-id="tag-LbcXXuZb">
-      </img></div>
-    </div>
-    <div class="" tag-id="tag-DzslJVj5">
-      test adding tag-id
-    </div>
-
-    <Accordion type="single" collapsible="" tag-id="tag-XcsQEwvC">
-      <AccordionItem value="item-1" tag-id="tag-jmGpXcGA">
-        <AccordionTrigger tag-id="tag-xqMsLm7M">Is it accessible?</AccordionTrigger>
-        <AccordionContent tag-id="tag-CscHsiCN">
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
-
     <NuxtPage tag-id="tag-Xt3h6fyE"/>
   </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-  <div tag-id="tag-emTMqK16">index</div>
+  <div class="pt-safe bg-blue-400 p-2" tag-id="tag-LwLddRu5">
+    <div tag-id="tag-CHYIGDnb">level 1</div>
+    <div tag-id="tag-H1GF8SUG">
+      <span tag-id="tag-hvfmKtUz">level 2</span>
+      <div tag-id="tag-rzBmtjqq">level 3</div>
+      <img src="https://via.placeholder.com/150" tag-id="tag-LbcXXuZb">
+    </img></div>
+  </div>
+  <div class="" tag-id="tag-DzslJVj5">
+    test adding tag-id
+  </div>
+
+  <Accordion type="single" tag-id="tag-XcsQEwvC">
+    <AccordionItem value="item-1" tag-id="tag-jmGpXcGA">
+      <AccordionTrigger tag-id="tag-xqMsLm7M">Is it accessible?</AccordionTrigger>
+      <AccordionContent tag-id="tag-CscHsiCN">
+        Yes. It adheres to the WAI-ARIA design pattern.
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
 </template>
