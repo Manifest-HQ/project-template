29cdb1ced3761e0bb7e2361a2b048c00da836bb3
Eduardo López <loama18@gmail.com>
2024-04-11 18:20:38 +0200

    remove automatic updates for now

--- app/stores/Info.js
+++ app/stores/Info.js
@@ -1,8 +1,28 @@
 import { defineStore } from 'pinia'
-// import supabase from '@/supabase'
+import supabase from '@/supabase'
 
-export const useStoreInfo = defineStore('Info', () => {
-  const tables = ref(['a', 'b', 'c'])
+export const useStoreInfo = defineStore('Info', async () => {
+  const tables = ref([])
+
+  const { data: tablesData, error } = await supabase
+    .from('public_tables')
+    .select('*')
+
+  if (error) {
+    console.error('Error fetching tables:', error)
+  } else {
+    tables.value = tablesData
+  }
+
+  supabase.channel('custom-all-channel')
+    .on(
+      'postgres_changes',
+      { event: '*', schema: 'public', table: 'mouse_pointers' },
+      (payload) => {
+        console.log(payload)
+      }
+    )
+    .subscribe()
 
   return { tables }
 })
