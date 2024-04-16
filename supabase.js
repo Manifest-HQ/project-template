1600c903eea523fd6e3d535d0824f6e20e1600af
Eduardo López <loama18@gmail.com>
2024-04-16 23:15:26 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
 // SUPABASE instance of the project
-const supabaseUrl = '{{SUPABASE_URL}}'
+const supabaseUrl = 'https://qrofpkndepgbhcdgalpc.supabase.co'
 // this is the PUBLIC KEY, so it is safe to have it hard coded here
-const supabaseKey = '{{SUPABASE_KEY}}'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyb2Zwa25kZXBnYmhjZGdhbHBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NzExNzcsImV4cCI6MjAyODQ0NzE3N30.zXAHy9OttCEh4x4D1_OhWQfqP_TCqovvftkXLWz8qKA'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
