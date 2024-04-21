2dcd6ca1d72ef4f413703ddd680a646aa89601d4
Eduardo López <loama18@gmail.com>
2024-04-21 20:15:54 +0200

    change

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="pt-safe bg-blue-400 p-2" tag-id="tag-LwLddRu5">
+  <div class="pt-safe bg-green-600 p-2" tag-id="tag-LwLddRu5">
     <div tag-id="tag-CHYIGDnb">level 1</div>
     <div tag-id="tag-H1GF8SUG">
-      <span tag-id="tag-hvfmKtUz">level 2</span>
       <div tag-id="tag-rzBmtjqq">level 3</div>
       <img src="https://via.placeholder.com/150" tag-id="tag-LbcXXuZb">
     </img></div>
