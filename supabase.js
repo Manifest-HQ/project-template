ed896e791fa52555aa48b535bbfd0d3edf7bdd66
Eduardo López <loama18@gmail.com>
2024-04-23 15:49:24 +0200

    fix default values for supabase key and url

--- supabase.js
+++ supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
 // SUPABASE instance of the project
-const supabaseUrl = 'https://qrofpkndepgbhcdgalpc.supabase.co'
+const supabaseUrl = '{{SUPABASE_URL}}'
 // this is the PUBLIC KEY, so it is safe to have it hard coded here
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyb2Zwa25kZXBnYmhjZGdhbHBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NzExNzcsImV4cCI6MjAyODQ0NzE3N30.zXAHy9OttCEh4x4D1_OhWQfqP_TCqovvftkXLWz8qKA'
+const supabaseKey = '{{SUPABASE_KEY}}'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
