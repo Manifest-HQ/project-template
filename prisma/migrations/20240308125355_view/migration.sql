e84fa1f093258f2a193cd23c492c17b5cbc2de2a
Eduardo López <loama18@gmail.com>
2024-07-16 18:18:12 +0200

    show table names in public_tables view

--- prisma/migrations/20240308125355_view/migration.sql
+++ prisma/migrations/20240308125355_view/migration.sql
@@ -1,10 +1,13 @@
 CREATE OR REPLACE VIEW public_tables AS 
-SELECT 
-  table_schema, 
-  table_name 
-FROM 
-  information_schema.tables 
-WHERE 
-  table_schema = 'public' 
-  AND table_name NOT LIKE '\_%' ESCAPE '\'
-  AND table_type = 'BASE TABLE';
+SELECT tables.table_schema,
+       tables.table_name,
+       '[' || string_agg('"' || columns.column_name || '"', ',') || ']' AS column_names
+FROM information_schema.tables AS tables
+JOIN information_schema.columns AS columns
+  ON tables.table_schema = columns.table_schema
+  AND tables.table_name = columns.table_name
+WHERE tables.table_schema = 'public'
+  AND tables.table_name NOT LIKE '\_%'
+  AND tables.table_type = 'BASE TABLE'
+GROUP BY tables.table_schema, tables.table_name;
+
