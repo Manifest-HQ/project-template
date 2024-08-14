67cc572aa81309faa362e72108431166d08e5b57
Esteban Amaya <esteban.amaya.fonseca@gmail.com>
2024-08-14 15:26:09 -0500

    added a server

---
+++ server/functions/install.js
@@ -0,0 +1,99 @@
+import fs from 'fs'
+import { exec } from 'child_process'
+
+const packageJsonPath = '../app/package.json'
+const dir = '../app'
+let packageJsonContent = ''
+let serverProcess = null
+
+function startServer() {
+  serverProcess = exec(
+    'bun i && bun run dev',
+    { cwd: dir },
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error starting the server: ${error.message}`)
+        return
+      }
+      if (stderr) {
+        console.error(`Server error: ${stderr}`)
+        return
+      }
+      console.log(`Server started: ${stdout}`)
+    }
+  )
+}
+
+export function stopServer(port = process.env.port || '3004') {
+  exec(`fuser -k ${port}/tcp`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error stopping the server on port ${port}: ${error}`)
+      return
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`)
+      return
+    }
+    console.log(`Server stopped on port ${port}`)
+  })
+}
+
+packageJsonContent = fs.readFileSync(packageJsonPath, 'utf8')
+
+fs.watchFile(packageJsonPath, (curr, prev) => {
+  console.log('Change detected in package.json')
+
+  const prevPackageJson = JSON.parse(packageJsonContent)
+  const currPackageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
+
+  const prevDependencies = prevPackageJson.dependencies || {}
+  const currDependencies = currPackageJson.dependencies || {}
+  const prevDevDependencies = prevPackageJson.devDependencies || {}
+  const currDevDependencies = currPackageJson.devDependencies || {}
+
+  const newDependencies = Object.keys(currDependencies).filter(
+    (dep) => !(dep in prevDependencies)
+  )
+  const newDevDependencies = Object.keys(currDevDependencies).filter(
+    (dep) => !(dep in prevDevDependencies)
+  )
+
+  if (newDependencies.length > 0 || newDevDependencies.length > 0) {
+console.log(
+  'New dependencies detected:',
+  newDependencies.concat(newDevDependencies)
+)
+    stopServer()
+
+    const dependenciesToInstall = newDependencies.map(
+      (dep) => `${dep}@${currDependencies[dep]}`
+    )
+    const devDependenciesToInstall = newDevDependencies.map(
+      (dep) => `${dep}@${currDevDependencies[dep]}`
+    )
+
+    const allDependenciesToInstall = dependenciesToInstall
+      .concat(devDependenciesToInstall)
+      .join(' ')
+    console.log(`toinstall: ${allDependenciesToInstall}`)
+    exec(`bun i `, { cwd: dir }, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error installing new dependencies: ${error}`)
+        return
+      }
+      console.log('start server')
+      startServer()
+    })
+  } else {
+    console.log('No new dependencies. The server continues running.')
+  }
+
+  // Actualizar el contenido de packageJsonContent con el nuevo contenido
+  packageJsonContent = fs.readFileSync(packageJsonPath, 'utf8')
+})
+
+// Iniciar el servidor por primera vez
+console.log('Init server')
+startServer()
+
+export default stopServer
