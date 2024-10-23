d2e4cef2ccbd71722ca8302d93bad911d6f53356
Eduardo López <loama18@gmail.com>
2024-10-23 02:04:14 +0200

    change var

--- app/supabase.js
+++ app/supabase.js
@@ -1,13 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// const supabaseUrl = '{{SUPABASE_URL}}'
-// const supabaseKey = '{{SUPABASE_KEY}}'
-
-// SUPABASE instance of the project
-const supabaseUrl = 'https://qvpgximhadpefbykykgl.supabase.co'
-// PUBLIC KEY, it is safe to have it hard coded here, it is not secret
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2cGd4aW1oYWRwZWZieWt5a2dsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjczODE4NjcsImV4cCI6MjA0Mjk1Nzg2N30.B9xjjY1vZWxhxkLOd3xQflL2vrRuNihLTQ20xplV4_I'
+const supabaseUrl = '{{SUPABASE_URL}}'
+const supabaseKey = '{{SUPABASE_PUBLIC_KEY}}'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
--- supabase.js
+++ supabase.js
@@ -2,16 +2,9 @@ import { createClient } from '@supabase/supabase-js'
 
 // SUPABASE instance of the project
 const supabaseUrl = '{{SUPABASE_URL}}'
-// this is the PUBLIC KEY, so it is safe to have it hard coded here
-const supabaseKey = '{{SUPABASE_KEY}}'
+const supabasePublicKey = '{{SUPABASE_PUBLIC_KEY}}'
 
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-const supabaseManifestSchema = createClient(supabaseUrl, supabaseKey, {
-  db: {
-    schema: 'manifest'
-  }
-})
+const supabase = createClient(supabaseUrl, supabasePublicKey)
 
 const supabaseManifestDB = createClient(
   // Manifest HQ Main Supabase URL
@@ -23,4 +16,4 @@ const supabaseManifestDB = createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4dXNiZmVtenRxaXBxdmxuZWF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU4NjUyMDgsImV4cCI6MjAxMTQ0MTIwOH0.xlpgbon7iolkJa6YB-E1hXHQFyDdN4FDVZBg6To0sos'
 )
 
-export { supabase, supabaseManifestSchema, supabaseManifestDB }
+export { supabase, supabaseManifestDB }
